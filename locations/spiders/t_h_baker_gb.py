a66e85f95a62908174624173d9d89ba9cdf4c5af
TheUKHighStreet <122377165+TheUKHighStreet@users.noreply.github.com>
2024-09-25 17:06:40 +0100

    Create t_h_baker_gb.py

---
+++ locations/spiders/t_h_baker_gb.py
@@ -0,0 +1,16 @@
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+
+from locations.structured_data_spider import StructuredDataSpider
+
+
+class THBakerGBSpider(StructuredDataSpider):
+    name = "t_h_baker_gb"
+    item_attributes = {"brand": "T H Baker", "brand_wikidata": "Q28406430"}
+    start_urls = [
+        "https://www.thbaker.co.uk/stores/",
+    ]
+    rules = [
+        Rule(LinkExtractor(r"https://www.thbaker.co.uk/stores/([-\w]+)$"), "parse"),
+    ]
+
